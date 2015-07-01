c70fb442ff272edc242f309985fa2b34bec7085f
alex3493 <alex3493@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-01 11:28:44 +0000

    wrong files
    git-svn-id: https://plugins.svn.wordpress.org/smart-countdown-fx-easy-recurring-events/tags/1.3@1190740 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/plural_js/plural_fr_fr.js
+++
@@ -1,7 +0,0 @@
-function smartcountdown_plural(n) {
-	if(n == 1 || n == 0) {
-		return '_1';
-	} else {
-		return '';
-	}
-}
--- js/vendor/timepicker.collapse.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function() {
-	jQuery(document).on('click', '.t-datepicker', function(e) {
-		jQuery(this).datetimepicker('destroy').datetimepicker({
-			dateFormat : 'yy-mm-dd',
-			timeFormat: 'HH:mm:ss'
-		}).focus();
-	});
-});
